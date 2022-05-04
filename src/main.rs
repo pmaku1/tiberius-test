df2d6bd44c9a6cc1320e02ae5546fa0ba9e9d408
Patrick Maku <amakupatrick@gmail.com>
2022-05-04 05:44:39 -0500

    add tracing to debug TLS connection issue

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 mod provider_model;
 use provider_model::NYProviderFeed;
-use serde_json::Value;
-use tracing::log::info;
 use std::error::Error;
+use std::process;
 use tiberius::{AuthMethod, Client, Config, Query};
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
-use std::process;
+use tracing::{error, info};
+use tracing_subscriber;
 
-
+static INPUT_FILE: &str = "/Users/patrickmaku/Projects/tiberius-test/provider-test-data.csv";
 
 #[tokio::main]
-async fn main() -> Result<(),Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
+    // tracing_subscriber::fmt::init()
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+    info!("Application starting ...");
 
     if let Err(err) = example().await {
-        println!("error running example: {}", err);
+        error!("error running example: {}", err);
         process::exit(1);
     }
     Ok(())
 }
 
-
 async fn example() -> Result<(), Box<dyn Error>> {
+    info!("Configure database connection ...");
+
     let mut config = Config::new();
 
     config.host("localhost");
     config.port(1433);
-    config.authentication(AuthMethod::sql_server("SA", "Password_01"));
+    config.authentication(AuthMethod::sql_server("sa", "Password_01"));
     config.trust_cert(); // on production, it is not a good idea to do this
 
     let tcp = TcpStream::connect(config.get_addr()).await?;
@@ -35,18 +41,19 @@ async fn example() -> Result<(), Box<dyn Error>> {
     // To be able to use Tokio's tcp, we're using the `compat_write` from
     // the `TokioAsyncWriteCompatExt` to get a stream compatible with the
     // traits from the `futures` crate.
+
     let mut client = Client::connect(config, tcp.compat_write()).await?;
+    let mut rdr = csv::Reader::from_path(INPUT_FILE)?;
 
-    // let mut rdr = csv::ReaderBuilder::new()
-    //     .has_headers(true)
-    //     .from_reader(io::stdin());
+    info!("Start loading database ...");
 
-    let mut rdr = csv::Reader::from_path("/home/pmaku/projects/provider-test-data.csv")?;
-    for result in rdr.deserialize(){
+    for result in rdr.deserialize() {
         let record: NYProviderFeed = result?;
-        let id_insert = client.execute("SET  IDENTITY_INSERT provider.dbo.ProvMaster ON",&[] ).await;
-        print!("{:#?}",id_insert);
-        let mut query = Query::new("INSERT INTO provider.dbo.ProvMaster (ProvMasterKey) VALUES (@P1), (@P2), (@P3),(@p4), (@P5), (@P6),(@P7),(@P8), (@P9), (@P10), (@P11), (@P12), (@P13), (@P14),(@P15),(@P16),(@P18),(@P19)");
+        // let id_insert = client.execute("SET  IDENTITY_INSERT provider.dbo.ProvMaster ON",&[] ).await;
+
+        // let mut query = Query::new("INSERT INTO provider.dbo.ProvMaster (ProvMasterKey) VALUES (@P1), (@P2), (@P3),(@p4), (@P5), (@P6),(@P7),(@P8), (@P9), (@P10), (@P11), (@P12), (@P13), (@P14),(@P15),(@P16),(@P18),(@P19)");
+        let mut query = Query::new("INSERT INTO provider.dbo.ProvMaster  VALUES (@P1), (@P2), (@P3),(@p4), (@P5), (@P6),(@P7),(@P8), (@P9), (@P10), (@P11), (@P12), (@P13), (@P14),(@P15),(@P16),(@P18),(@P19)");
+
         query.bind(record.medicaid_provider_id);
         query.bind(record.npi);
         query.bind(record.provider_or_facility_name);
@@ -68,9 +75,7 @@ async fn example() -> Result<(), Box<dyn Error>> {
         query.bind(record.provider_email);
         let _results = query.execute(&mut client).await;
 
-        println!("{:#?}",_results);
+        println!("{:#?}", _results);
     }
     Ok(())
 }
-
-
